f5ad38bfc68cc33bf95f8e9f4b1dab4414f8b37d
hansonjw <hansonjw@gmail.com>
2020-08-06 20:45:38 -0500

    Add randomness to player and robot attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -25,7 +25,7 @@ var fight = function(enemyName) {
       if (confirmSkip) {
         window.alert(playerName + ' has decided to skip this fight. Goodbye!');
         // subtract money from playerMoney for skipping
-        playerMoney = playerMoney - 10;
+        playerMoney = Math.max(0, playerMoney - 10);
         console.log("playerMoney", playerMoney)
         break;
       }
@@ -33,7 +33,10 @@ var fight = function(enemyName) {
 
     // If 'skip' is not inputed, default is to fight
     // remove enemy's health by subtracting the amount set in the playerAttack variable
-    enemyHealth = enemyHealth - playerAttack;
+    // generate random damage value based on player's attack power
+    var damage = randomNumber(playerAttack - 3, playerAttack);
+    enemyHealth = Math.max(0, enemyHealth - damage);
+
     console.log(
       playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.'
     );
@@ -53,7 +56,9 @@ var fight = function(enemyName) {
     }
 
     // remove players's health by subtracting the amount set in the enemyAttack variable
-    playerHealth = playerHealth - enemyAttack;
+    var damage = randomNumber(enemyAttack - 3, enemyAttack);
+    playerHealth = Math.max(0, playerHealth - damage);
+    
     console.log(
       enemyName + ' attacked ' + playerName + '. ' + playerName + ' now has ' + playerHealth + ' health remaining.'
     );
@@ -83,7 +88,7 @@ var shop = function(){
         window.alert("Refilling player's health by 20 for 7 dollars.");
   
         playerHealth = playerHealth + 20;
-        playerMoney = playerMoney - 7;
+        playerMoney = Math.max(0, playerMoney - 7);
       }
       else {
         window.alert("You don't have enough money!");
@@ -97,7 +102,7 @@ var shop = function(){
 
       // increase attack and decrease money
         playerAttack = playerAttack + 6;
-        playerMoney = playerMoney - 7;
+        playerMoney = math.max(0, playerMoney - 7);
       }
       else {
         window.alert("You don't have enough money!");
@@ -120,6 +125,13 @@ var shop = function(){
   }
 };
 
+// Function to generate a random number when needed
+var randomNumber = function(min, max) {
+  var value = Math.floor(Math.random() * (max - min +1) + min);
+
+  return value;
+};
+
 var startGame = function(){
   //Reset player stats/variables
   //I do not think this is working as intended...
@@ -132,7 +144,7 @@ var startGame = function(){
     if (playerHealth > 0) {
       alert("Welcome to Robot Gladiators! Round " + ( i + 1 ) );
       var pickedEnemyName = enemyNames[i];
-      enemyHealth = 50;
+      enemyHealth = randomNumber(40, 60);
       fight(pickedEnemyName);
 
       if (playerHealth > 0 && i < enemyNames.length - 1){
